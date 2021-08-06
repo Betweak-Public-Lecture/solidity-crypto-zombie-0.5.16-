683a8a6b777887c7ac8a9f87d6577e4d1b9bc252
betweak12 <ys@betweak.com>
2021-08-06 13:42:04 +0900

    lesson1-11

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -18,4 +18,25 @@ contract ZombieFactory {
         // zombies.push(zombie);
         zombies.push(Zombie(_name, _dna));
     }
+
+    // _generateRandomDna라는 private 함수를 만들고 인자는 (string _str) 반환타입은 (uint)
+    // 이 함수는 컨트랙트 변수를 보지만 변경하지는 않을 것이므로 view로 선언.
+    function _generateRandomDna(string _str) private view returns (uint256) {
+        // 함수내용은 _str을 keccak256 해시값(fake random)을 구해서 uint256 rand에 저장
+        // 해당 rand값을 16자리로 만들어서 반환 (hint: rand % ?
+        // 545612348 --> 5자리의 난수로 만들려고 한다. 545612348 % 100000 ( 10**5)
+
+        uint256 rand = keccak256(_str);
+        return rand % dnaModulus;
+    }
+
+    // function sampleFunc() public view returns (stirng, uint256) {
+    //     Zombie zombie = zombies[0];
+    //     return (zombie.name, zombie.dna);
+    // }
+
+    // function sampleFunc2() public view {
+    //     string name;
+    //     (name, ) = sampleFunc();
+    // }
 }
