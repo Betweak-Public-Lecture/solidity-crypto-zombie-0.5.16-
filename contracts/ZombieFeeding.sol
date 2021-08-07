05aa4b86bbfe19aa4ab79300e24c15ebcfcc208f
betweak12 <ys@betweak.com>
2021-08-07 13:22:01 +0900

    final lesson2

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -24,25 +24,29 @@ contract ZombieFeeding is ZombieFactory {
     address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
     KittyInterface kittyInterface = KittyInterface(ckAddress);
 
-    function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
+    function feedAndMultiply(
+        uint256 _zombieId,
+        uint256 _targetDna,
+        string _species
+    ) public {
         // 2. 주인만이 먹이를 줄 수 있도록 구성(require문 활용)
         require(msg.sender == zombieToOwner[_zombieId]);
         // 3. 먹이를 먹는 대상 좀비 가져오기
         Zombie storage zombie = zombies[_zombieId];
 
         uint256 newDna = (zombie.dna + _targetDna) / 2;
+        if (
+            keccak256(abi.encodePacked(_species)) ==
+            keccak256(abi.encodePacked("kitty"))
+        ) {
+            newDna = newDna - (newDna % 100) + 99;
+        }
         _createZombie("NoName", newDna);
     }
 
     function feedOnKitty(uint256 _zombieId, uint256 _kittyId) public {
         uint256 kittyDna;
         (, , , , , , , , , kittyDna) = kittyInterface.getKitty(_kittyId);
-        feedAndMultiply(_zombieId, kittyDna);
+        feedAndMultiply(_zombieId, kittyDna, "kitty");
     }
 }
-/**
-feedOnKitty 함수 생성 (uint _zombieId, uint _kittyId) public
-2. 함수내에서 uint kittyDna 변수 선언
-3. _kittyID를 전달하여 kittyInterface에 getKitty함수 호출
-4. genes를 kittyDna 에 저장
-5. feedAndMultiply함수 호출  _zombieId와 kittyDna 전달 */
